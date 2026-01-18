3514b4fd5c4b46e0bf165eff2231d31159ca5d87
bingwa <munyaobryan@gmail.com>
2026-01-18 15:11:50 +0300

    v10

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,7 +1,6 @@
 import { getProductsOnSale } from '@/lib/products'
 import ProductCard from '@/components/product/ProductCard'
 
-export const dynamic = 'force-dynamic'
 
 export default async function SalePage() {
   const products = await getProductsOnSale()
